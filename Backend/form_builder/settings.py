9a2e9d0fe4909e548558ae35a4c9876ebf465205
DebdipWritesCode <debdipmukherjee52@gmail.com>
2024-12-30 11:17:28 +0530

    add CORS for Frontend integration

--- Backend/form_builder/settings.py
+++ Backend/form_builder/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "corsheaders",
     "forms",
 ]
 
@@ -57,6 +58,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173", # Development frontend
 ]
 
 ROOT_URLCONF = "form_builder.urls"
