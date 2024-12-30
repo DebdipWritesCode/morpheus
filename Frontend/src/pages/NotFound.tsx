c76ae31cb81c1e0636f1acbabf8dce41d7d540ef
DebdipWritesCode <debdipmukherjee52@gmail.com>
2024-12-30 15:27:20 +0530

    add not found page

--- Frontend/src/pages/NotFound.tsx
+++ Frontend/src/pages/NotFound.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="flex flex-col gap-10 items-center justify-center h-screen">
+      <h1 className="text-5xl font-bold text-red-600">404 Page Not Found</h1>
+      <Link to={"/"}>
+        <button className="px-3 py-2 bg-slate-100 font-semibold text-red-600 border border-red-600 rounded-3xl hover:bg-slate-200">
+          Go back to home
+        </button>
+      </Link>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
