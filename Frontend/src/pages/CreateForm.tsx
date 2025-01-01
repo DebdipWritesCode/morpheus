1dbcd31010962d27d234918fd87515ac2bf45e59
DebdipWritesCode <debdipmukherjee52@gmail.com>
2025-01-01 23:03:15 +0530

    add drag and drop for reordering of questions

--- Frontend/src/pages/CreateForm.tsx
+++ Frontend/src/pages/CreateForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Text from "../components/QuestionTypes/Text";
 import Checkbox from "../components/QuestionTypes/Checkbox";
 import Dropdown from "../components/QuestionTypes/Dropdown";
@@ -17,6 +17,20 @@ import PercentageAllocation from "../components/QuestionTypes/PercentageAllocati
 
 import { FaRegTrashAlt } from "react-icons/fa";
 import { PiDotsSixBold } from "react-icons/pi";
+import {
+  draggable,
+  dropTargetForElements,
+} from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import { setCustomNativeDragPreview } from "@atlaskit/pragmatic-drag-and-drop/element/set-custom-native-drag-preview";
+import { pointerOutsideOfPreview } from "@atlaskit/pragmatic-drag-and-drop/element/pointer-outside-of-preview";
+import {
+  attachClosestEdge,
+  type Edge,
+  extractClosestEdge,
+} from "@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge";
+import { DropIndicator } from "@atlaskit/pragmatic-drag-and-drop-react-drop-indicator/box";
+
+import { v4 as uuidv4 } from "uuid";
 
 interface QuestionTypes {
   text: string;
@@ -37,10 +51,12 @@ interface QuestionTypes {
 }
 
 interface Question {
-  id: number;
+  id: string;
   type: keyof QuestionTypes;
   question_text: string;
   options?: {};
+  elementRef: React.RefObject<HTMLDivElement>;
+  buttonRef: React.RefObject<HTMLButtonElement>;
 }
 
 const questionComponents: { [key in keyof QuestionTypes]: React.FC } = {
@@ -67,6 +83,9 @@ const CreateForm = () => {
   const [selectedQuestionType, setSelectedQuestionType] =
     useState<keyof QuestionTypes>("text");
   const [questions, setQuestions] = useState<Question[]>([]);
+  const [closestEdge, setClosestEdge] = useState<Edge | null>(null);
+  const [currentDraggingQuestionID, setCurrentDraggingQuestionID] =
+    useState<string>("");
 
   const handleFormTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormTitle(e.target.value);
@@ -91,9 +110,11 @@ const CreateForm = () => {
       setQuestions((prevQuestions) => [
         ...prevQuestions,
         {
-          id: prevQuestions.length,
+          id: uuidv4(),
           type: selectedQuestionType,
           question_text: "New Question",
+          elementRef: React.createRef<HTMLDivElement>(),
+          buttonRef: React.createRef<HTMLButtonElement>(),
         },
       ]);
     } else {
@@ -103,13 +124,146 @@ const CreateForm = () => {
 
   const handleDeleteQuestion = (
     e: React.MouseEvent<HTMLButtonElement>,
-    id: number
+    id: string
   ) => {
     e.preventDefault();
     setQuestions((prevQuestions) =>
       prevQuestions.filter((question) => question.id !== id)
     );
   };
+
+  useEffect(() => {
+    questions.forEach((question) => {
+      const element = question.elementRef.current;
+      const btn = question.buttonRef.current;
+
+      if (
+        element &&
+        btn &&
+        !element.hasAttribute("draggable") &&
+        !btn.hasAttribute("draggable")
+      ) {
+        draggable({
+          element: btn,
+          getInitialData: () => ({ id: question.id }),
+          onGenerateDragPreview: ({ nativeSetDragImage }) => {
+            setCustomNativeDragPreview({
+              nativeSetDragImage,
+              getOffset: pointerOutsideOfPreview({
+                x: "-60px",
+                y: "5px",
+              }),
+              render({ container }) {
+                const el = element.cloneNode(true) as HTMLDivElement;
+                const computedStyle = window.getComputedStyle(element);
+                el.style.width = computedStyle.width;
+                el.style.height = computedStyle.height;
+
+                container.appendChild(el);
+              },
+            });
+          },
+        });
+
+        dropTargetForElements({
+          element: element,
+          canDrop({ source }) {
+            if (source.data.id && source.data.id !== question.id) {
+              return true;
+            }
+            return false;
+          },
+          getData({ input }) {
+            return attachClosestEdge(
+              { id: question.id },
+              {
+                element,
+                input,
+                allowedEdges: ["top", "bottom"],
+              }
+            );
+          },
+          onDrag({ self, source }) {
+            const isSource = source.data.id === question.id;
+            if (isSource) {
+              setClosestEdge(null);
+              return;
+            }
+
+            const closestEdge = extractClosestEdge(self.data);
+
+            const selfID = self.data.id as string;
+            setCurrentDraggingQuestionID(selfID);
+
+            setClosestEdge(closestEdge);
+          },
+          onDragLeave() {
+            setClosestEdge(null);
+          },
+          onDrop({ self, source }) {
+            const questionID = self.data.id as string;
+            const sourceID = source.data.id as string;
+            const closestEdge = extractClosestEdge(self.data);
+
+            setQuestions((prevQuestions) => {
+              const sourceIndex = prevQuestions.findIndex(
+                (question) => question.id === sourceID
+              );
+              const targetIndex = prevQuestions.findIndex(
+                (question) => question.id === questionID
+              );
+
+              const newQuestions = [...prevQuestions];
+              if (closestEdge === "top" && sourceIndex > targetIndex) {
+                const firstHalf = newQuestions.slice(0, targetIndex);
+                firstHalf.push(newQuestions[sourceIndex]);
+                const secondHalf = newQuestions.slice(targetIndex, sourceIndex);
+                const thirdHalf = newQuestions.slice(sourceIndex + 1);
+                return [...firstHalf, ...secondHalf, ...thirdHalf];
+              } else if (
+                closestEdge === "bottom" &&
+                sourceIndex > targetIndex
+              ) {
+                const firstHalf = newQuestions.slice(0, targetIndex + 1);
+                firstHalf.push(newQuestions[sourceIndex]);
+                const secondHalf = newQuestions.slice(
+                  targetIndex + 1,
+                  sourceIndex
+                );
+                const thirdHalf = newQuestions.slice(sourceIndex + 1);
+                return [...firstHalf, ...secondHalf, ...thirdHalf];
+              } else if (closestEdge === "top" && sourceIndex < targetIndex) {
+                const firstHalf = newQuestions.slice(0, sourceIndex);
+                const secondHalf = newQuestions.slice(
+                  sourceIndex + 1,
+                  targetIndex
+                );
+                secondHalf.push(newQuestions[sourceIndex]);
+                const thirdHalf = newQuestions.slice(targetIndex);
+                return [...firstHalf, ...secondHalf, ...thirdHalf];
+              } else if (
+                closestEdge === "bottom" &&
+                sourceIndex < targetIndex
+              ) {
+                const firstHalf = newQuestions.slice(0, sourceIndex);
+                const secondHalf = newQuestions.slice(
+                  sourceIndex + 1,
+                  targetIndex + 1
+                );
+                secondHalf.push(newQuestions[sourceIndex]);
+                const thirdHalf = newQuestions.slice(targetIndex + 1);
+                return [...firstHalf, ...secondHalf, ...thirdHalf];
+              } else {
+                return newQuestions;
+              }
+            });
+
+            setClosestEdge(null);
+          },
+        });
+      }
+    });
+  }, [questions]);
 
   return (
     <div className=" flex justify-center mt-5 py-5 px-4">
@@ -156,8 +310,14 @@ const CreateForm = () => {
             const QuestionComponent = questionComponents[question.type];
             return (
               <div
+                ref={question.elementRef}
                 key={question.id}
-                className="relative px-4 pb-3 pt-5 border mt-8 rounded-lg">
+                className={
+                  "relative px-4 pb-3 pt-5 border mt-8 rounded-lg" +
+                  (closestEdge && currentDraggingQuestionID === question.id
+                    ? " bg-slate-100 opacity-60"
+                    : "")
+                }>
                 <QuestionComponent key={question.id} />
                 <div className=" absolute -top-5 -left-10 flex gap-4 items-center translate-x-1/2 w-full">
                   <button
@@ -166,11 +326,15 @@ const CreateForm = () => {
                     <FaRegTrashAlt size={22} />
                   </button>
                   <button
+                    ref={question.buttonRef}
                     className="border rounded-full p-2 bg-white hover:bg-slate-200"
-                    onClick={(e) => handleDeleteQuestion(e, question.id)}>
+                    onClick={(e) => e.preventDefault()}>
                     <PiDotsSixBold size={22} />
                   </button>
                 </div>
+                {closestEdge && currentDraggingQuestionID === question.id && (
+                  <DropIndicator edge={closestEdge} gap="1px" />
+                )}
               </div>
             );
           })}
